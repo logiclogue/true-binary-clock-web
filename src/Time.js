7c756009b4b77671fb351e2870f5f7d11d99c792
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-02 18:12:32 +0100

    Fixes Time class to pass tests

--- src/Time.js
+++ src/Time.js
@@ -2,7 +2,7 @@
  * Provides the daily time in binary format.
  */
 function Time() {
-    this.data = new Date();
+    this.date = new Date();
     this.decimal;
 }
 
@@ -12,6 +12,7 @@ function Time() {
      * Gets the time as a decimal.
      */
     proto_.getDecimal = function () {
+        var date = this.date;
         var milli = date.getMilliseconds() / 86400000;
         var second = date.getSeconds() / 86400;
         var minute = date.getMinutes() / 1440;
@@ -27,26 +28,23 @@ function Time() {
      */
     proto_.getBinary = function () {
         var array = [];
-        var decimalTime = this.decimal;
+        var decimalTime = this.getDecimal();
         var pow;
         var isOn;
         var i;
         var j;
 
-        for (i = 0; i < 16; i += 1) {
+        for (i = 1; i <= 16; i += 1) {
             pow = Math.pow(2, i);
             isOn = false;
 
             for (j = 1; j < pow; j += 2) {
-                if (decimalTime > j / pow && decimalTime < (j + 1) / pow) {
-                    array[i] = true;
+                if (decimalTime >= j / pow && decimalTime < (j + 1) / pow) {
                     isOn = true;
                 }
             }
 
-            if (!isOn) {
-                array[i - 1] = false;
-            }
+            array[i - 1] = isOn;
         }
 
         return array;
