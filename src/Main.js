f1c77976627fd0467321dbfdb1998a033fdd892b
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-04 13:46:42 +0100

    Updates Main class to actually make the clock work

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,35 @@
-function Main() {
+var ClockDOM = require('./ClockDOM');
+var Time = require('./Time');
+var SizeManager = require('./SizeManager');
 
+
+function Main() {
+    this.time = new Time();
+    this.clock = new ClockDOM();
+    this.sizeManager = new SizeManager();
 }
 
+(function (static_, proto_) {
+
+    proto_.start = function () {
+        this.time.getBinary();
+
+        setInterval(this._interval.bind(this), 80);
+    };
+
+    proto_._interval = function () {
+        this.time.date = new Date();
+        var binaryTime = this.time.getBinary();
+
+        this.sizeManager.update();
+        this.clock.updateTime(binaryTime);
+        this.clock.draw();
+    };
+
+}(Main, Main.prototype));
+
 (function () {
-    new Main();
+    var main = new Main();
+
+    main.start();
 }());
